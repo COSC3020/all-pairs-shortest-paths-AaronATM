bc1115521af2b8aadcffb3f093f7075cc54fb8eb
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-07 14:14:20 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
-function allPairsShortestPaths(graph) {
-    return [];
+// used code from geeks for geeks
+// https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/#
+
+// input is weighted, directed adjacency matrix
+function allPairsShortestPaths(graph) 
+{
+    var dist = Array.from(Array(graph.length), () => new Array(graph.length).fill(0));
+
+    for (var i = 0; i < graph.length; i++)
+    {
+        for (var j = 0; j < graph.length; j++)
+        {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (var i = 0; i < graph.length; i++)
+    {
+        for (var j = 0; j < graph.length; j++)
+        {
+            for (var k = 0; k < graph.length; k++)
+            {
+                if (dist[j][i] + dist[i][k] < dist[j][k])
+                {
+                    dist[j][k] = dist[j][i] + dist[i][k];
+                }
+            }
+        }
+    }
+
+    return dist;
 }
