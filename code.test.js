7bb835c4f3885d85c872eee1b13c07dcfc570d1b
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-07 14:14:14 -0600

    added test cases

--- code.test.js
+++ code.test.js
@@ -1,4 +1,7 @@
 // based on test code from my detecting isomorphism exercise
+// test cases from:
+// https://www.programiz.com/dsa/floyd-warshall-algorithm
+// https://www.tutorialspoint.com/data_structures_algorithms/floyd_warshall_algorithm.htm
 
 const fs = require('fs');
 const JSVerify = require('jsverify');
@@ -7,9 +10,41 @@ const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 const testset = [
-    { graph: [], expected: []},
-    { graph: [], expected: []},
-    { graph: [], expected: []},
+    { graph: [
+        [0, 5, Infinity, 10],
+        [Infinity, 0, 3, Infinity],
+        [Infinity, Infinity, 0, 1],
+        [Infinity, Infinity, Infinity, 0],
+      ], expected: [
+        [0, 5, 8, 9],
+        [Infinity, 0, 3, 4],
+        [Infinity, Infinity, 0, 1],
+        [Infinity, Infinity, Infinity, 0]
+    ]},
+    { graph: [
+        [0, 3, Infinity, 5],
+        [2, 0, Infinity, 4],
+        [Infinity, 1, 0, Infinity],
+        [Infinity, Infinity, 2, 0]
+    ], expected: [
+        [0, 3, 7, 5],
+        [2, 0, 6, 4],
+        [3, 1, 0, 5],
+        [5, 3, 2, 0]
+    ]},
+    { graph: [
+        [0, 5, Infinity, 6, Infinity],
+        [Infinity, 0, 1, Infinity, 7],
+        [3, Infinity, 0, 4, Infinity],
+        [Infinity, Infinity, 2, 0, 3],
+        [2, Infinity, Infinity, 5, 0]
+    ], expected: [
+        [0, 5, 6, 6, 9],
+        [4, 0, 1, 5, 7],
+        [3, 8, 0, 4, 7],
+        [5, 10, 2, 0, 3],
+        [2, 7, 7, 5, 0]
+    ]},
 ];
 
 testset.forEach(({ graph, expected }, index) => {
